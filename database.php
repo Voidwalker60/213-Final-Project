af61f70a0582376fd5050cc1a5d42d07cd635c73
Maddie <voidwalker604@protonmail.com>
2025-10-29 15:04:12 -0700

    DB definition with table creation

--- database.php
+++ database.php
@@ -1,29 +1,65 @@
 <?php
+//Variables to simplify connection
 $serverHost = "127.0.0.1";
 $serverUser = "root";
-$serverPass = "";
+$serverPass = "password";
 
 $sqlConnect = mysqli_connect($serverHost, $serverUser, $serverPass);
+//If we cannot connect, kill
 if (!$sqlConnect) {
     die();
 }
 $sqlConnect->set_charset("utf8");
+//createDB is used to improve readability
 $createDB = "CREATE DATABASE IF NOT EXISTS eventDatabase";
 
-if(mySqli_query($sqlConnect, $createDB) === TRUE) {
-    echo "Database created successfully";
-}
-else
-{
-    echo "Error Creating";
-}
-$createTB = "CREATE TABLE IF NOT EXISTS eventTB
+//Connect to DB
+mysqli_select_db($sqlConnect, "eventDatabase");
+
+//tableSchemas contains all table definitions for easy iteration
+$tableSchemas = [
+    //Event schema
+    "CREATE TABLE IF NOT EXISTS eventTB
 (
     id INT(6) PRIMARY KEY AUTO_INCREMENT,
-    eventName VARCHAR(50) NOT NULL,
-    eventType VARCHAR(50) NOT NULL
-)";
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    date DATE NOT NULL,
+    startTIme TIME NOT NULL,
+    endTime TIME NOT NULL,
+    description VARCHAR(255) NOT NULL
+    
+)",
+    //User schema
+    "CREATE TABLE IF NOT EXISTS userTB
+(
+    id INT(6) PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    isAdmin TINYINT(1) NOT NULL,
+    email VARCHAR(50) NOT NULL
+)",
+    //Bookings schema
+    "CREATE TABLE IF NOT EXISTS bookingTB(
+    
+    id INT(6) PRIMARY KEY AUTO_INCREMENT,
+    userID INT(6) NOT NULL,
+    eventID INT(6) NOT NULL
+)
+"
 
-mysqli_select_db($sqlConnect, "eventDatabase");
+];
+
+//Iterate over all tables, and create them. Print an error if impossible
+for ($i = 0; $i < count($tableSchemas); $i++) {
+    if (mySqli_query($sqlConnect, $tableSchemas[$i]) === TRUE) {
+        echo "Table " . $i . " created successfully";
+    } else {
+        echo "Error Creating table: " . $i;
+    }
+}
+
+
 mysqli_close($sqlConnect);
 ?>
